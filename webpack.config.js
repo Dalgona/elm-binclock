dbcd51811877a4febb8b416b338cf163688efa18
Dalgona <dalgona@hontou.moe>
2017-06-22 21:42:37 +0900

    Apply uglifyjs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 'use strict';
 
+var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+
   module: {
     rules: [
       {
@@ -32,6 +35,10 @@ module.exports = {
       }
     ],
     noParse: /\.elm$/
-  }
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 };
 
